8338571177dc6e65f8db865d99ef5d1eb3bd9df8
nchronas <nchronas@gmail.com>
2015-11-13 16:58:45 +0200

    adding pygame

--- start.sh
+++ start.sh
@@ -3,3 +3,20 @@
 export PASSWD=${PASSWD:=root}
 #Set the root password
 echo "root:$PASSWD" | chpasswd
+
+echo "Hello"
+
+udevd --daemon
+udevadm trigger
+
+if [ ! -c /dev/fb1 ]; then
+  echo "loading piTFT kernel module"
+  modprobe spi-bcm2708
+  modprobe fbtft_device name=pitft verbose=0 rotate=270
+
+  sleep 1
+
+  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ')
+fi
+
+sleep 2
